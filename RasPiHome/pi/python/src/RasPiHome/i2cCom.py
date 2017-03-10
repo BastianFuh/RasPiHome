e5c2155620e15be5e60acda36ed34fd967fa35db
Baeshen <bastian.fuhlenriede@gmail.com>
2017-03-10 08:56:13 +0100

    Project restructure still going on

--- RasPiHome/pi/python/src/RasPiHome/i2cCom.py
+++ RasPiHome/pi/python/src/RasPiHome/i2cCom.py
@@ -1,26 +1,4 @@
 import smbus
-import time
-
-# CONST - FUNCTION CODES 
-
-
-DEBUG = 0
-
-
-# -- READ
-
-
-
-# -- WRITE
-
-# SERVO START
-SERVO_WRITE = 128
-# SERVO END
-
-
-#
-
-bus = None
 
 bus = smbus.SMBus(1)
 
@@ -28,63 +6,43 @@ def setup():
 	global bus 
 	bus = smbus.SMBus(1)
 
-# could be removed, because it may be not needed
-def writeByte(addr, fun_code):
-	try:
-		bus.write_byte(addr, value)
-	except IOError, err:
-		return -1
-	return 0	
-
-
-def readByte():
-	None
-
-
 
 # ----------------------------------------------------------------------
 # Writes data to a certain device over the i2c bus
 #  
 # addr     	- the i2c address of the device
-# fun_code 	- code of the function that should be executed
-#	           see "CONST - FUNCTION CODES" at the beginning of the code
+# port	 	- port of the interface
 # value		- additional information in an array 
 # ----------------------------------------------------------------------
 
-def writeData(addr, fun_code, val):
+def writeData(addr, port, val):
 	try:
-		bus.write_i2c_block_data(addr, fun_code, val)
+		bus.write_i2c_block_data(addr, port, val)
 	except IOError, err:
 		return -1
 	return 0
 
-	
+
 # ----------------------------------------------------------------------
 # Read data from a certain device over the i2c bus
 #
 # addr		- the i2c address of the device
-# fun_code 	- code of the function that should be executed
-#	           see "CONST - FUNCTION CODES" at the beginning of the code
+# port	 	- port of the interface
 # ----------------------------------------------------------------------
 
-def readData(addr, fun_code):
+def readData(addr, port):
+	data  	= 0
+	counter = 0
+
 	try:
-		data = bus.read_i2c_block_data(addr, fun_code)
-		
+		buffer = bus.read_i2c_block_data(addr, port)
+
+		length = buffer[0]
+		while counter < length:
+			data += ( buffer[counter + 2] << (8 * length - 1 - counter ))
 	except IOError, err:
 		return -1
 	return data
 
 
-while True:
-	time.sleep(5)
-	
-	tmp = bus.read_i2c_block_data(0x08, 2)
-	tmp = bus.read_i2c_block_data(0x08, 2)
-	print "Humidity: ", (tmp[0] << 8) + tmp[1]
-	
 
-	tmp = bus.read_i2c_block_data(0x08, 1)
-	print "Temperature: ", (tmp[0] << 8) + tmp[1]  
-	
-	
